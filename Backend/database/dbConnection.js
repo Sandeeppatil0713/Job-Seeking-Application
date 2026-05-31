fb062e68aba6ae68c139fc0446ce3bed006d60f9
Sandeeppatil0713 <155381282+Sandeeppatil0713@users.noreply.github.com>
2026-05-31 19:08:57 +0530

    Login Error Solved

--- Backend/database/dbConnection.js
+++ Backend/database/dbConnection.js
@@ -3,11 +3,12 @@ import mongoose from 'mongoose';
 
  export const dbConnection = () =>{
     mongoose.connect(process.env.MONGO_URI, {
-    dbName:"JOB_SEEKING_APPLICATION",
+        dbName: "JOB_SEEKING_APPLICATION",
+        serverSelectionTimeoutMS: 10000,
+        family: 4,
     }).then(()=>{
         console.log('Database connected Successfully');
     }).catch((err)=>{
-        console.log(`Some error ouccered while connecting to database : ${err}`)   
+        console.log(`Some error occurred while connecting to database : ${err}`)   
     });
-
 };
